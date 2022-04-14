309486c57765f950025dbc8da962fedc7e526417
Alexander-Sobolev <sobolev.ios.developer@gmail.com>
2022-04-14 07:03:48 +0300

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Alexander Sobolev on 14.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
